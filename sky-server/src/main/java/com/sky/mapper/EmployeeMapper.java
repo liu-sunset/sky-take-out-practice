ab431415e499005c6eca1c69ba300350da536fc8
liu-sunset <liulooklandscape@outlook.com>
2025-06-17 10:34:20 +0800

    1. 完成了员工的分页查询开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -20,4 +21,7 @@ public interface EmployeeMapper {
     @Insert("insert into employee(name, username, password, phone, sex, id_number,status, create_time, update_time, create_user, update_user)" +
             "values(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
+
+    //分页查询
+    Page<Employee> selectEmpPageMapper();
 }
